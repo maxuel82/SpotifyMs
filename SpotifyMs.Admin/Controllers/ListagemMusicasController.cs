74fc2a7b49c4e59d8291bd3b4456df4829dffe05
Maxuel82 <maxuel82@gmail.com>
2024-07-06 11:10:37 -0300

    Implementando relatorio de listagem de musisas

---
+++ SpotifyMs.Admin/Controllers/ListagemMusicasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpotifyMs.Aplication.Streaming.Dto;
+using SpotifyMs.Aplication.Streaming;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SpotifyMs.Admin.Controllers
+{
+
+    [Authorize]
+    public class ListagemMusicasController : Controller
+    {
+        private MusicaService musicaService;
+
+        public ListagemMusicasController(MusicaService musicaService)
+        {
+            this.musicaService = musicaService;
+        }
+
+        public IActionResult Index()
+        {
+            var result = this.musicaService.GetAll();
+            return View(result);
+        }
+
+    }
+}
