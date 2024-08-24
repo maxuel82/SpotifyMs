01ea7408e63dd335ebd8a58d7c86dcd7fc5ee922
Maxuel82 <maxuel82@gmail.com>
2024-08-24 14:47:05 -0300

    habilitando Swagger para publicação

--- SpotifyMs.Api/Program.cs
+++ SpotifyMs.Api/Program.cs
@@ -88,11 +88,11 @@ builder.Services.AddScoped<PlaylistService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
